cfc907ea9e9dd9afbed1167b5ed46d888f37e2bc
sique <sique@HP-MAYRA>
2024-10-11 21:57:03 -0700

    Agregar archivos de proyecto.

---
+++ CALIFICACION/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Evaluacion_Calificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Console.Write("Ingresa la calificacion: ");
+            int calificacion = int.Parse(Console.ReadLine());
+
+            if (calificacion >= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"La calificacion es de {calificacion}, aprobado.");
+            }
+            else if (calificacion >= 50)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"La calificacion es de {calificacion}, reprobado con derecho a regularizacion.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"La calificacion es de {calificacion}, reprobado debe volver a cursar.");
+            }
+
+
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
